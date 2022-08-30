b7818952e4f7a3068ac4b60741be93d4359c9b22
Sohail Mehmood <sohail.mehmood@worldline.com>
2022-08-30 13:29:49 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class AllPrime
+{
+    static bool isPrime(int n)
+    {
+        if (n == 0 || n == 1)
+            return false;
+
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    static void Main(String[] args)
+    {
+        int N = int.MaxValue;
+
+        for (int i = 1; i <= N; i++)
+
+            if (isPrime(i))
+            {
+                Console.WriteLine(i + " ");
+            }
+    }
+}
