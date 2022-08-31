2997519ee660d9a2dfc3027fe41f2dae92109d35
Sohail Mehmood <sohail.mehmood@worldline.com>
2022-08-31 17:13:01 +0100

    Added namespace

--- Program.cs
+++ Program.cs
@@ -1,30 +1,33 @@
 ﻿using System;
 
-class AllPrime
+namespace AllPrime
 {
-    static bool isPrime(int number)
+    class AllPrime
     {
-        if (number == 0 || number == 1)
-            return false;
-
-        for (int i = 2; i < number; i++)
+        static bool isPrime(int number)
         {
-            if (number % i == 0)
+            if (number == 0 || number == 1)
                 return false;
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
         }
 
-        return true;
-    }
+        static void Main(string[] args)
+        {
+            int max = int.MaxValue;
 
-    static void Main(String[] args)
-    {
-        int max = int.MaxValue;
+            for (int i = 1; i <= max; i++)
 
-        for (int i = 1; i <= max; i++)
-
-            if (isPrime(i))
-            {
-                Console.WriteLine(i + " ");
-            }
+                if (isPrime(i))
+                {
+                    Console.WriteLine(i + " ");
+                }
+        }
     }
 }
