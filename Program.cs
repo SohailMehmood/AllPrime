b6a0142aa29ab68ff0bb155b534cff1989ee5dd6
Sohail Mehmood <sohail.mehmood@worldline.com>
2022-09-01 10:35:39 +0100

    Added access modifiers to class and methods

--- Program.cs
+++ Program.cs
@@ -2,32 +2,38 @@
 
 namespace AllPrime
 {
-    class AllPrime
+    public static class AllPrime
     {
-        static bool isPrime(int number)
+        public static void Main(string[] args)
+        {
+            const int MAX = int.MaxValue;
+
+            for (int i = 1; i <= MAX; i++)
+            {
+                bool isPrime = IsPrime(i);
+                if (isPrime)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        } 
+        
+        private static bool IsPrime(int number)
         {
             if (number == 0 || number == 1)
+            {
                 return false;
+            }
 
             for (int i = 2; i < number; i++)
             {
                 if (number % i == 0)
+                {
                     return false;
+                }
             }
 
             return true;
-        }
-
-        static void Main(string[] args)
-        {
-            int max = int.MaxValue;
-
-            for (int i = 1; i <= max; i++)
-
-                if (isPrime(i))
-                {
-                    Console.WriteLine(i + " ");
-                }
         }
     }
 }
